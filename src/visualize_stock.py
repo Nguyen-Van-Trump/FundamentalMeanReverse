1d938017ae6c1d2a5ccee94be9ee4ad45fa5a993
Nguyen-Van-Trump <trung1682000@gmail.com>
2026-03-13 15:30:46 +0700

    misc.

--- src/visualize_stock.py
+++ src/visualize_stock.py
@@ -8,21 +8,21 @@ import sys
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.append(str(PROJECT_ROOT))
 
-from config.settings import MARKET_DATA_DIR, TIME_COLUMN
+from config.settings import MARKET_DATA_DIR, RATIO_DATA_DIR, TIME_COLUMN
 
 
 def load_symbol(symbol: str):
 
-    file = MARKET_DATA_DIR / f"symbol={symbol}" / "data.parquet"
+    file = RATIO_DATA_DIR / f"symbol={symbol}" / "data.parquet"
 
     if not file.exists():
         raise FileNotFoundError(f"No data found for {symbol}")
 
     df = pd.read_parquet(file)
 
-    df[TIME_COLUMN] = pd.to_datetime(df[TIME_COLUMN])
+    # df[TIME_COLUMN] = pd.to_datetime(df[TIME_COLUMN])
 
-    df = df.sort_values(TIME_COLUMN)
+    # df = df.sort_values(TIME_COLUMN)
 
     return df
 
@@ -32,17 +32,20 @@ def info(symbol: str):
     df = load_symbol(symbol)
 
     print(f"\nLoaded {symbol}")
-    print(f"Rows: {len(df)}")
-    print(f"Date range: {df[TIME_COLUMN].min()} -> {df[TIME_COLUMN].max()}")
+    print(f"Shape: {df.shape}")
+    print("Columns\n")
+    print(df.columns)
+    # print(f"Date range: {df[TIME_COLUMN].min()} -> {df[TIME_COLUMN].max()}")
 
     print("\nSample data:")
     print(df.tail())
 
     # plt.figure(figsize=(12,6))
 
-    # plt.plot(df[TIME_COLUMN], df["close"], label="Close Price")
+    # # plt.plot(df[TIME_COLUMN], df["P/E"], label="P/E")
+    # plt.plot(df["P/E"])
 
-    # plt.title(f"{symbol} Close Price")
+    # plt.title(f"{symbol} P/E")
     # plt.xlabel("Date")
     # plt.ylabel("Price")
     # plt.grid(True)
